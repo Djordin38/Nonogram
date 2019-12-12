b84b2fb8b058f89608598ba7871d8e5c76a10c6c
Djordin38 <39299386+Djordin38@users.noreply.github.com>
2019-12-12 14:40:16 +0100

    Update Vakje.java

--- Vakje.java
+++ Vakje.java
@@ -1,4 +1,29 @@
-package Nonogram;
+package eersteVersie;
 
 public class Vakje {
+
+    private int x;
+    private int y;
+
+
+    public Vakje(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
